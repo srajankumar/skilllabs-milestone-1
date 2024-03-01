8a8f95c5152975776e3c77855e07a10624b6e5d7
srajan <kumarsrajan02@gmail.com>
2024-03-01 16:22:04 +0530

    some basics done

---
+++ src/routes/recipes.js
@@ -0,0 +1,65 @@
+import express from "express";
+import mongoose from "mongoose";
+import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
+import { verifyToken } from "./users.js";
+
+const router = express.Router();
+
+// Retrieve all recipes from the database
+router.get("/", async (req, res) => {
+  try {
+    const response = await RecipeModel.find({});
+    res.json(response);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Create a new recipe using data from the request body
+router.post("/", verifyToken, async (req, res) => {
+  const recipe = new RecipeModel(req.body);
+  try {
+    const response = await recipe.save();
+    res.json(response);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Add a recipe to a user's list of saved recipes
+router.put("/", verifyToken, async (req, res) => {
+  try {
+    const recipe = await RecipeModel.findById(req.body.recipeID);
+    const user = await UserModel.findById(req.body.userID);
+    user.savedRecipes.push(recipe);
+    await user.save();
+    res.json({ savedRecipes: user.savedRecipes });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Fetch saved recipe IDs for a user
+router.get("/addRecipes/ids/:userID", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userID);
+    res.json({ savedRecipes: user?.savedRecipes });
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// Fetch and return the actual saved recipes for a user
+router.get("/myRecipes/:userID", async (req, res) => {
+  try {
+    const user = await UserModel.findById(req.params.userID);
+    const savedRecipes = await RecipeModel.find({
+      _id: { $in: user.savedRecipes },
+    });
+    res.json({ savedRecipes });
+  } catch (err) {
+    res.json(err);
+  }
+});
+export { router as recipesRouter };
